933c8c6fea534f6b8bf7cb8b0f6474aff8cf4e3f
SomayaAhmed1430 <s.ramadan02185@dmu.edu.eg>
2025-06-01 09:51:30 +0300

    Add project files.

---
+++ LaptopStore/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using LaptopStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LaptopStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductBL productBL = new ProductBL();
+        //URL "Product/All"
+        public IActionResult All()
+        {
+            //1) model get data
+            List<Product> PList = productBL.GetAll();
+            //2) send to view
+
+            return View("ShowAll", PList);
+        }
+
+        //URL "Product/ProductDetails/1"
+        //URL "Product/ProductDetails?id=1"
+        public IActionResult ProductDetails(int id) 
+        {
+            Product PModel = productBL.GetById(id);
+            return View("ProductDetails", PModel);
+        }
+    }
+
+}
---
+++ LaptopStore/Models/ProductBL.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace LaptopStore.Models
+{
+    public class ProductBL
+    {
+        // DB
+        List<Product> products;
+        public ProductBL()
+        {
+            products = new()
+            {
+                new Product() {Id = 1, Name = "Dell", Price = 10500, Img = "4.jpg"},
+                new Product() {Id = 2, Name = "Hp", Price = 15900, Img = "2.jpg"},
+                new Product() {Id = 3, Name = "Lenovo", Price = 25400, Img = "3.jpg"},
+                new Product() {Id = 4, Name = "ThinkPad", Price = 17500, Img = "4.jpg"},
+                new Product() {Id = 5, Name = "MacBook", Price = 36500, Img = "5.jpg"},
+                new Product() {Id = 6, Name = "Asuc ", Price = 10000, Img = "6.jpg"},
+            };
+        }
+        public List<Product> GetAll()
+        {
+            return products;
+        }
+        public Product GetById(int id)
+        {
+            return products.FirstOrDefault(p=>p.Id == id);
+        }
+    }
+}
